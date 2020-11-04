8ed83a48c17f4d373b4b4fbd7c6281e0c55e3d62
comPC <outfoxed@163.com>
2020-11-04 17:37:43 +0800

    简单网络提交
    简单网络提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,35 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '-----',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    pickerHidden: true,
+    chosen: ''
+  },
+  sendData(){
+    wx.request({
+      url: 'http://127.0.0.1:5000/api/companies', //仅为示例，并非真实的接口地址
+      method:'POST',
+      data: {
+        name: 'from wechat mp',
+        country: "China",
+        industry: "Internet",
+        Product: "Facebook",
+        Introduction:"122222123123123123"
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      fail(res){
+        console.log('')
+        console.log(res)
+      },
+      success (res) {
+        console.log(res)
+      }
+    })
   },
   //事件处理函数
   bindViewTap: function() {
@@ -35,6 +60,8 @@ Page({
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
+          console.log(res);
+          
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
@@ -49,6 +76,44 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+  onShareAppMessage() {
+    return {
+      title: 'form',
+      path: 'page/component/pages/form/form'
+    }
+  },
+  pickerConfirm(e) {
+    this.setData({
+      pickerHidden: true
+    })
+    this.setData({
+      chosen: e.detail.value
+    })
+  },
+
+  pickerCancel() {
+    this.setData({
+      pickerHidden: true
+    })
+  },
+
+  pickerShow() {
+    this.setData({
+      pickerHidden: false
+    })
+  },
+
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
     })
   }
 })
